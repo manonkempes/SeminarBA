fc480f20624c02147797bc88e3b34eb356f72205
noor4148 <noorvduuren@gmail.com>
2026-03-25 21:46:00 +0100

    hidden_dim = 64

--- train_retrieval.py
+++ train_retrieval.py
@@ -102,7 +102,7 @@ if __name__ == '__main__':
     parser.add_argument('--seed', type=int, default=21)
     parser.add_argument('--epochs', type=int, default=200)
     parser.add_argument('--gpu_num', type=int, default=0)
-    parser.add_argument('--batch_size', type=int, default=64)
+    parser.add_argument('--batch_size', type=int, default=128)
     parser.add_argument('--val_batch_size', type=int, default=32)
     parser.add_argument('--num_workers', type=int, default=0)
     parser.add_argument('--check_val_every_n_epoch', type=int, default=5)
